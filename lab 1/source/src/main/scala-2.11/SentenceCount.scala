7544d357f00d379f41a211083be388bb00f25ac3
Dig Vijay Kumar Yarlagadda <y.digvijaykumar@gmail.com>
2016-08-31 16:00:48 -0500

    build successful

--- lab 1/source/src/main/scala-2.11/SentenceCount.scala
+++ lab 1/source/src/main/scala-2.11/SentenceCount.scala
@@ -8,16 +8,15 @@ object SentenceCount {
   def main(args: Array[String]): Unit = {
     System.setProperty("hadoop.home.dir","C:\\winutils")
     System.setProperty("spark.executor.memory","8G")
-    val conf = new SparkConf().setAppName("SparkSentenceCount").setMaster("local[*]").set("spark.executor.memory","12g").set("spark.hadoop.validateOutputSpecs", "false") //Overwrite output files
+    val conf = new SparkConf().setAppName("SparkSentenceCount").setMaster("local[*]").set("spark.executor.memory","8g").set("spark.hadoop.validateOutputSpecs", "false") //Overwrite output files
 
     val sc = new SparkContext(conf)
     val textFile: RDD[String] = sc.textFile("data/input/sentencesFile.txt")
 
-
     val sentenceCount=textFile.map(s=>(s,1)).reduceByKey((a,b)=>(a+b)).coalesce(1).saveAsTextFile("data/output/sentenceCount.txt")
 
     val sortedSentences=textFile.map(s=>(s,1)).reduceByKey((a,b)=>a+b).sortByKey().coalesce(1).saveAsTextFile("data/output/sortedSentences.txt")
-    println("jj")
+
     val totalNumSentences=textFile.map(line => line).count()
     println(s"There are $totalNumSentences sentences in the input file")
 
