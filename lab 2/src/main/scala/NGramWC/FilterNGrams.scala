22a957b084cc39f7f04285c90590034344c545ca
Dig Vijay Kumar Yarlagadda <y.digvijaykumar@gmail.com>
2016-09-07 22:26:59 -0500

    Generate NGrams and Filter NGrams

--- lab 2/src/main/scala/NGramWC/FilterNGrams.scala
+++ lab 2/src/main/scala/NGramWC/FilterNGrams.scala
@@ -1,16 +1,16 @@
 package main.scala.NGramWC
 
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
   * Created by digvijayky on 9/7/2016.
   */
-
-import org.apache.spark.rdd.RDD
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.log4j.Logger
-import org.apache.log4j.Level
-
-object NGramMR {
+object FilterNGrams {
   def main(args: Array[String]): Unit = {
+    val n = 4
+    val filterWord = "MIT"
+
     System.setProperty("hadoop.home.dir","C:\\winutils")
     System.setProperty("spark.executor.memory","8G")
     val conf = new SparkConf().setAppName("SparkSentenceCount").setMaster("local[*]").set("spark.executor.memory","8g").set("spark.hadoop.validateOutputSpecs", "false") //Overwrite output files
@@ -20,18 +20,11 @@ object NGramMR {
     val sc = new SparkContext(conf)
     val textFile = sc.textFile("lab 2/data/input/sentencesFile.txt").toLocalIterator;
 
-    val n = 4
-    println("%"*100+"\n"+s"Generating ${n}-grams from the input file"+"\n"+"%"*100+"\n")
+    println("%" * 100 + "\n" + s"Generating ${n}-grams from the input file" + "\n" + "%" * 100 + "\n")
     val ngrams = textFile
       .map(_.split("\\s").toList)
       .flatMap(_.sliding(n))
-      .filter(_.size==n)
+      .filter(_.size == n)
       .foreach(println)
-
-
-
-
-
   }
 }
-
